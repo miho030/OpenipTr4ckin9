dc248c39d17f0b4486199ab33863bc314faf13a1
nicht <miho0_0@naver.com>
2019-06-24 15:36:11 +0900

    Update IP SRC Location Tracking Script.Ver0.1.py

--- OpenipTr4ckin9.Ver.0.1/IP SRC Location Tracking Script.Ver0.1.py
+++ OpenipTr4ckin9.Ver.0.1/IP SRC Location Tracking Script.Ver0.1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Author : Repubic of Korea, Seoul, JungSan HS  31227 Lee Joon Sung
+# Author : Repubic of Korea, Seoul, JungSan HS  31227
 # Author_Helper : Republic of Korea, KyungGido, Kim Min Seok
 # youtube : anonymous0korea0@gmail.com ;;;; tayaka
 # Email : miho0_0@naver.com
